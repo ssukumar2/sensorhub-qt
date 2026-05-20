7dd3b9267c9a596950c69a1b34a2b7d63099b213
S Sukumar <s.sukumar.eu@gmail.com>
2026-05-20 20:49:52 +0200

    Add AppContext/NotificationBanner/HealthDock hooks to MainWindow

--- inc/mainwindow.h
+++ inc/mainwindow.h
@@ -6,10 +6,19 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 
+namespace sensorhub {
+class AppContext;
+class NotificationBanner;
+class HealthDock;
+class KeyboardShortcuts;
+}
+
 class MainWindow : public QMainWindow 
 {
     Q_OBJECT
 public:
+    void setAppContext(sensorhub::AppContext *ctx);
+
     explicit MainWindow(QWidget* parent = nullptr);
 
 private slots:
@@ -22,4 +31,15 @@ private:
     QTimer* timer_;
     QNetworkAccessManager* net_;
     QString backendUrl_ = "http://localhost:8000";
+
+private:
+    sensorhub::AppContext         *m_appCtx        = nullptr;
+    sensorhub::NotificationBanner *m_banner        = nullptr;
+    sensorhub::HealthDock         *m_healthDock    = nullptr;
+    sensorhub::KeyboardShortcuts  *m_shortcuts     = nullptr;
+
+private slots:
+    void onShortcutTriggered(const QString &id);
+    void onSensorWentStale(const QString &id, const QString &name);
+    void onSensorRecovered(const QString &id, const QString &name);
 };
